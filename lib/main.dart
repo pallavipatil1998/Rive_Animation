e5a132345955feca7b4d82859a1afebf78ba04c2
Pallavi Patil <pallavishravan545@gmail.com>
2024-07-01 21:26:07 +0530

    Rive Animation

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,18 @@ class RiveHomePage extends StatefulWidget {
 class _RiveHomePageState extends State<RiveHomePage> {
 
   final rivePath="assets/rive/star_animation.riv";
+  final loginBearPath="assets/rive/loginBear.riv";
   StateMachineController? mController ;
   Artboard? mArtBoard;
   SMIInput<bool>? riveInput;
+  SMIBool? isChecking;
+  SMIBool? isHandsUp;
+  SMITrigger? trigSuccess;
+  SMITrigger? trigFail;
+
+
+  var emailController=TextEditingController();
+  var passController=TextEditingController();
 
 
   @override
@@ -48,8 +57,8 @@ class _RiveHomePageState extends State<RiveHomePage> {
     _initializeRive();
 
 
-      }
-      
+  }
+
 
 
 
@@ -60,13 +69,17 @@ class _RiveHomePageState extends State<RiveHomePage> {
   }
 
   Future<void> _loadRiveFile()async{
-    await rootBundle.load(rivePath).then((riveByteData) {
+    await rootBundle.load(loginBearPath).then((riveByteData) {
       var file = RiveFile.import(riveByteData);
       var artboard = file.mainArtboard;
-      mController = StateMachineController.fromArtboard(artboard, "StarFav");
+      mController = StateMachineController.fromArtboard(artboard, "Login Machine");
       if (mController != null) {
         artboard.addController(mController!);
-        riveInput = mController!.findInput("check");
+        // riveInput = mController!.findInput("check");
+        isChecking=mController!.findSMI("isChecking");
+        isHandsUp=mController!.findSMI("isHandsUp");
+        trigSuccess=mController!.findSMI("trigSuccess");
+        trigFail=mController!.findSMI("trigFail");
         setState(() {
           mArtBoard = artboard;
         });
@@ -78,27 +91,96 @@ class _RiveHomePageState extends State<RiveHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Rive Animation"),),
-      body: mArtBoard==null ? Container(width: 100,height: 100,color: Colors.blue,):
-          Column(children: [
-            InkWell(
-              onTap: (){
-                if(riveInput!= null){
-                  if(riveInput!.value==false  && riveInput!.controller.isActive==false){
-                    riveInput!.value=true;
-                  }else if(riveInput!.value==true && riveInput!.controller.isActive==false){
-                    riveInput!.value=false;
-                  }
-                }
-              },
-              child: SizedBox(height: 200,width: 200,
-                  child: Rive(artboard: mArtBoard!)),
+        appBar: AppBar(title: Text("Rive Animation"),),
+        body: mArtBoard==null ? Container(width: 100,height: 100,color: Colors.blue,):
+            Padding(
+              padding: const EdgeInsets.all(11.0),
+              child: Column(
+                children: [
+                  Expanded(child: Rive(artboard: mArtBoard!)),
+                  SizedBox(height: 50),
+                  TextField(
+                    decoration: InputDecoration(
+                      label: Text("Enter Email")
+                    ),
+                    onTap: (){
+                      isChecking!.value=false;
+                      isHandsUp!.value=false;
+                      },
+                    onChanged: (value){
+                      isChecking!.value=true;
+                      isHandsUp!.value=false;
+                    },
+                    onEditingComplete: (){
+                      isChecking!.value=false;
+                    },
+                    onTapOutside: (event){
+                      isChecking!.value=false;
+                    },
+                    onSubmitted: (event){
+                      isChecking!.value=false;
+                    },
+
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      label: Text("Enter Password")
+                    ),
+                    onTap: (){
+                      isChecking!.value=false;
+                      isHandsUp!.value=false;
+                    },
+                    onChanged: (value){
+                      isHandsUp!.value=true;
+                      isChecking!.value=false;
+                    },
+
+                    onTapOutside: (event){
+                      isHandsUp!.value=false;
+                      isChecking!.value=false;
+                    },
+                    onSubmitted: (event){
+                      isHandsUp!.value=false;
+                    },
+
+
+                  ),
+
+                  ElevatedButton(onPressed: (){
+                    var email=emailController.text.toString();
+                    var pass = passController.text.toString();
+
+                    if(email=="Admin" && pass=="12345"){
+
+                      trigSuccess!.value=true;
+                     /* if(isHandsUp!.value){     // if true
+                        isHandsUp!.value=false;
+                        trigSuccess!.value=true;
+                      }else{
+                        trigSuccess!.value=true;
+                      }*/
+                    }else {
+
+                      trigFail!.value=true;
+                     /* if(isHandsUp!.value){
+                        isHandsUp!.value=false;
+                        trigFail!.value=true;
+                      }else{
+                        trigFail!.value=true;
+                      }*/
+                    }
+
+
+                  },
+                      child: Text("Login")
+                  )
+                ],
+              ),
             )
-          ],)
+       
 
     );
   }
 }
-
 
 
